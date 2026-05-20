a804a9e531034274aa3cb712f3bdf3c46a90bfde
Alexander Gerhard <870717ag@gmail.com>
2026-05-20 12:24:59 +0200

    feat: filtredTodos, part 2

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,46 @@
-import express from 'express'
-import type { Request, Response } from 'express'
-import {Todo} from './models/Todo.js'
+import express from "express";
+import type { Request, Response } from "express";
+import { Todo } from "./models/Todo.js";
 
-const app = express()
+const app = express();
 
-const PORT = 5001
+const PORT = 5001;
 
-app.get('/ping', (_: Request, res: Response) => {
-    res.send('Helo world')
-})
+app.get("/ping", (_: Request, res: Response) => {
+  res.send("Helo world");
+});
 
-const todos: Todo [] = [
-    new Todo('Starta node server'),
-    new Todo('Felsöka node server'),
-    new Todo('Swisha Mrks för hjälp')
-]
+const todos: Todo[] = [
+  new Todo("Starta node server"),
+  new Todo("Felsöka node server"),
+  new Todo("Swisha Mrks för hjälp"),
+];
 
-app.get('/todos', (req: Request, res: Response) => {
-const search = req.query.search
-const sort = req.query.sort
+app.get("/todos", (req: Request, res: Response) => {
+  const search = req.query.search;
+  const sort = req.query.sort;
 
-let filtredTodos = todos
+  let filtredTodos = todos;
 
-if (search) {
-    filtredTodos = filtredTodos.filter((t) => t.content.includes(search.toString()))
-}
+  if (search) {
+    filtredTodos = filtredTodos.filter((t) =>
+      t.content.includes(search.toString()),
+    );
+  }
 
-    res.json(todos)
-})
+  if (sort && sort === "asc")
+    filtredTodos = filtredTodos.sort((a, b) => {
+      const todo1 = a.content.toLocaleLowerCase();
+      const todo2 = b.content.toLocaleLowerCase();
+
+      if (todo1 < todo2) return 1;
+      if (todo1 > todo2) return -1;
+      return 0; 
+    });
+
+  res.json(filtredTodos);
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-    
-})
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
