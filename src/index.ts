bdf77fa6eae278fb7f1f89dbe67aa82ea9d47f05
Alexander Gerhard <870717ag@gmail.com>
2026-05-21 13:06:55 +0200

    feat: new endpoints post, patch, delete

--- src/index.ts
+++ src/index.ts
@@ -38,18 +38,82 @@ app.get("/todos", (req: Request, res: Response) => {
 
       if (todo1 < todo2) return 1;
       if (todo1 > todo2) return -1;
-      return 0; 
+      return 0;
     });
 
   res.json(filtredTodos);
 });
 
-app.get('/todos/:id', (req: Request, res: Response) => {
-const id = req.params.id as string
-const todo = todos.find((t) => t.id === parseInt(id))
+app.get("/todos/:id", (req: Request, res: Response) => {
+  const id = req.params.id as string;
+  const todo = todos.find((t) => t.id === parseInt(id));
 
-res.json({todo})
-})
+  res.json({ todo });
+});
+
+app.post("/todos", (req: Request, res: Response) => {
+  try {
+    const content = req.body.content;
+
+    if (content === undefined) {
+      res.status(400).json({ error: "Content is required" });
+      return;
+    }
+
+    const newTodo = new Todo(content);
+    todos.push(newTodo);
+
+    res.status(201).json({ message: "Todo created" });
+  } catch (error) {
+    console.log("Error: ", error);
+    res.json({ error: error });
+  } finally {
+    console.log("This code will allways run");
+  }
+});
+
+app.patch("/todos/:id", (req: Request, res: Response) => {
+  const { content, done } = req.body;
+
+  if (content === undefined || done === undefined) {
+    res.status(400).json({ error: "Todo not found" });
+    return;
+  }
+
+  const todo = todos.find((t) => t.id === parseInt(req.params.id as string));
+  if (!todo) {
+    res.status(404).json({ error: "Todo not found" });
+    return;
+  }
+
+  todo.content = content;
+  todo.done = done;
+
+  res.json({ message: "Todo update", date: todo });
+});
+
+app.delete("/todos/:id", (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  if (!id) {
+    res.status(404).json({ error: "Todo not found" });
+    return;
+  }
+
+  const todoIndex = todos.findIndex(
+    (t) => t.id === parseInt(req.params.id as string),
+  );
+
+  const deletedTodo = todos[todoIndex];
+
+  todos.splice(todoIndex, 1);
+
+  res.json({
+    message: 'Todo deleted',
+    data: deletedTodo,
+  })
+
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
