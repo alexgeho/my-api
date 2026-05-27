3f6c945a6e8ab9dbe0f4b57f0665d28071d0af39
Alexander Gerhard <870717ag@gmail.com>
2026-05-27 18:54:33 +0200

    commit: create Post model with constructor and content preview limit

---
+++ src/models/Post.ts
@@ -0,0 +1,15 @@
+export class Post {
+  id: number = 0;
+  title: string = "";
+  author: string = "";
+  content: string = "";
+  date: string = "";
+
+  constructor(title: string, author: string, content: string) {
+    this.id = Math.round(Math.random() * 1000);
+    this.title = title;
+    this.author = author;
+    this.content = content.slice(0, 100);
+    this.date = new Date().toString();
+  }
+}
